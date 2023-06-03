4298fbbebd691af54a0ee0d72886dff95b63f2f8
esteban-florez <eflorez077@gmail.com>
2023-06-03 12:24:59 -0400

    installed daisyUI, configured ESLint and plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -14,5 +16,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    daisyui
+  ],
 }
