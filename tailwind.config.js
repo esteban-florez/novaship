ba66f4c12f148589314064cc4af7cc06763d824e
JesusMiguel03 <jmdefreitas10@gmail.com>
2023-06-11 14:15:50 -0400

    DaisyUI palette implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,42 @@
-import daisyui from 'daisyui'
+import daisyui from "daisyui"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        black: '#1d1916',
-        white: '#f3f4f6',
-        'gray-notification': '#8c8a9a',
-        'gray-divisor': '#413e3e',
-        'light-purple': '#c8c6dc',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        inter: ['var(--font-default)'],
-        title: ['var(--font-title)'],
+        inter: ["var(--font-default)"],
+        title: ["var(--font-title)"],
       },
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        custom_theme: {
+          primary: "#c8c6dc",
+          secondary: "#8c8a9a",
+          black: "#1d1916",
+          white: "#f3f4f6",
+          neutral: "#4b5563",
+          "base-100": "#323334",
+          info: "#2f72ee",
+          success: "#1c9c6d",
+          warning: "#fcd34d",
+          error: "#f03d19",
+        },
+      },
+    ],
   },
   plugins: [daisyui],
 }
