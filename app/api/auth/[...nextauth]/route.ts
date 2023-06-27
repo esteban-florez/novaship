1efec9989d8f7d7fb45f28da552ac9c7b3d7abee
esteban-florez <eflorez077@gmail.com>
2023-06-27 09:33:12 -0400

    added prisma adapter, moved auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,38 @@
-import authOptions from '@/auth/options'
 import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import prisma from '@/prisma/client'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { type NextAuthOptions } from 'next-auth'
+import { type Adapter } from 'next-auth/adapters'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize(credentials) {
+        console.log(credentials)
+        return {
+          id: '1',
+          name: 'Esteban Florez',
+          email: 'email@example.com',
+          image: null,
+        }
+      },
+    }),
+  ],
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions)
