3e9501b3c45af3dbb47d0a429ae62e144a522ed0
JesusMiguel03 <jmdefreitas10@gmail.com>
2023-06-27 01:43:32 -0400

    Button text fixed

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -161,7 +161,7 @@ export default function ProfilePage() {
           <LocationSection />
           <BiographySection />
           <div className="flex-center">
-            <button type="submit" className="btn-primary btn-sm btn mb-3 mt-4 w-full lg:btn-wide">Actualuzar datos</button>
+            <button type="submit" className="btn-primary btn-sm btn mb-3 mt-4 w-full lg:btn-wide">Actualizar datos</button>
           </div>
         </form>
       </section>
