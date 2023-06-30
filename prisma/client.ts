20aadb42c5e9a55e344a590ecae75edbb5b604dd
esteban-florez <eflorez077@gmail.com>
2023-06-30 12:49:19 -0400

    local prisma migrations

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,15 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-const devOptions = {
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-}
-
-const prisma = new PrismaClient(
-  process.env.NODE_ENV === 'development' ? devOptions : undefined,
-)
+const prisma = new PrismaClient()
 
 export default prisma
