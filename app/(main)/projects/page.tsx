82d428fd55cd2a22f14d27fb40599d13d4da2649
esteban-florez <eflorez077@gmail.com>
2023-07-04 09:43:41 -0400

    merge branch 'try' into dev

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -1,3 +1,7 @@
+import Archive from '@/components/projects/Archive'
+import ProjectDetails from '@/components/projects/ProjectDetails'
+import Filter from '@/components/projects/Filter'
+import TeamGroup from '@/components/projects/Team'
 import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -7,7 +11,19 @@ export const metadata: Metadata = {
 export default function ProjectsPage() {
   return (
     <>
-      <h2>Projects</h2>
+      <div className="my-4 grid grid-cols-1 gap-4 px-8 md:grid-cols-6">
+        <div className="md:col-span-4">
+          <ProjectDetails />
+          <Filter />
+          <Archive />
+        </div>
+        <div className="md:col-span-2">
+          <div className="flex flex-col gap-4">
+            <div className="flex h-80 flex-col gap-2 rounded-lg bg-neutral p-5" />
+            <TeamGroup />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
