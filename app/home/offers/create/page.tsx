92d6277ba2f1bfae1cd1cb2da7b7a0275ef9fbcd
MariYaqueno <myaqueno@gmail.com>
2023-08-13 19:39:16 -0400

    cambios frontend form

--- app/home/offers/create/page.tsx
+++ app/home/offers/create/page.tsx
@@ -1,5 +1,5 @@
-import PageTitle from '@/components/PageTitle'
 import CreateOfferForm from '@/components/offers-create/CreateOfferForm'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -8,9 +8,15 @@ export const metadata: Metadata = {
 
 export default function CreateOfferPage() {
   return (
-    <>
-      <PageTitle title="Crear oferta" />
+    <section className="container px-10 py-8">
+      <div className="relative flex h-28 items-center rounded-t-xl bg-primary px-6 py-4 text-white shadow">
+        <div className="z-20 flex items-center gap-1">
+          <PlusIcon className="h-9 w-9 font-bold" />
+          <h2 className="text-4xl font-bold tracking-tighter">Crear oferta</h2>
+        </div>
+        <img src="/coso3.webp" alt="Imagen decorativa en esquinas" className="absolute left-0 top-0 h-48 w-full rounded-t-xl" />
+      </div>
       <CreateOfferForm />
-    </>
+    </section>
   )
 }
--- components/offers/Carrousel.tsx
+++ components/offers/Carrousel.tsx
@@ -3,8 +3,6 @@
 import { useState } from 'react'
 import Btn from './Carrousel/Btn'
 import offers from './Carrousel/data.json'
-import Header from './Carrousel/Header'
-import Footer from './Carrousel/Footer'
 import Content from './Carrousel/Content'
 import Dots from './Carrousel/Dots'
 
@@ -22,14 +20,17 @@ export default function Carrousel() {
           className="flex w-full grow-0 flex-col"
         >
           <img src="https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagen de fondo carrusel" className="absolute right-0 top-0 h-full w-full object-cover" />
-          <div className="relative z-10 h-full w-full px-6 py-8 backdrop-blur-sm backdrop-brightness-50 sm:px-12">
-            <div className="absolute left-0 top-0 -z-10 h-full w-full rounded-tr-[15rem] bg-neutral-800 md:rounded-br-full md:rounded-tr-none lg:w-7/12" />
+          <div className="relative z-10 h-full w-full px-4 pt-6 backdrop-blur-sm backdrop-brightness-50 sm:px-0">
             <div className="w-full flex-row">
-              <Header title={offer.title} categories={offer.categories} />
-              <Content description={offer.description} />
-              <Footer owner={offer.owner} ubication={offer.ubication} />
+              <Content
+                title={offer.title}
+                categories={offer.categories}
+                description={offer.description}
+                owner={offer.owner}
+                ubication={offer.ubication}
+              />
             </div>
-            <div className="mt-4 flex w-full flex-row items-center justify-center gap-4">
+            <div className="my-4 flex w-full flex-row items-center justify-center gap-4">
               <div className="flex rounded-lg">
                 <Btn direction="left" onClick={() => { setCurrentSlide(previousSlide) }} />
                 <Dots length={offers.length} current={currentSlide} />
