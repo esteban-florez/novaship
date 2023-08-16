13168169b210f5a2fe354d01e8e314209e19d600
esteban-florez <eflorez077@gmail.com>
2023-08-16 08:26:19 -0400

    ts problems

--- components/projects-details/TeamGroupTabs.tsx
+++ components/projects-details/TeamGroupTabs.tsx
@@ -1,4 +1,3 @@
-import { UsersIcon } from '@heroicons/react/24/solid'
 import NavButton from '../NavButton'
 import { type TeamGroupTab } from '@/lib/types'
 
@@ -12,14 +11,12 @@ export default function TeamGroupTabs({ tab, setTab }: Props) {
     <div className="flex flex-col sm:flex-row">
       <NavButton
         isActive={tab === 'members'}
-        icon={<UsersIcon className="h-5 w-5" />}
         onClick={() => { setTab('members') }}
       >
         Miembros actuales
       </NavButton>
       <NavButton
         isActive={tab === 'add'}
-        icon={<UsersIcon className="h-5 w-5" />}
         onClick={() => { setTab('add') }}
       >
         Añadir miembros
