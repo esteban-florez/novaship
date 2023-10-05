7a52b82de184c565ef5cc41a0e966313397516f5
JesusMiguel03 <jmdefreitas10@gmail.com>
2023-10-05 10:32:51 -0400

    PageNav updated, new Dropdown component

--- components/projects/PageContent.tsx
+++ components/projects/PageContent.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import PageNav from './PageNav'
 import { useState } from 'react'
 import { ProjectsFull, type ProjectsTab } from '@/lib/types'
 import ProjectsCard from './ProjectsCard'
 import Pagination from '../Pagination'
 import PageTitle from '../PageTitle'
-import { PlusIcon } from '@heroicons/react/24/outline'
+import { GlobeAltIcon, ListBulletIcon, PlusIcon, StarIcon } from '@heroicons/react/24/outline'
 import Button from '../Button'
 import Link from 'next/link'
+import PageNav from '../PageNav'
+import clsx from 'clsx'
 
 type Props = React.PropsWithChildren<{
   filter: string | string[]
@@ -21,11 +22,32 @@ type Props = React.PropsWithChildren<{
 
 export default function PageContent({ filter, projects, personalProjects, suggestedProjects, nextPage, pageNumber }: Props) {
   const [search, setSearch] = useState('')
-  const PROJECT_TABS = {
+    const PROJECT_TABS = {
     all: projects,
     personal: personalProjects,
     suggested: suggestedProjects,
   }
+  const PROJECTS_TAB_TRANSLATION = {
+    all: 'Todos',
+    suggested: 'Proyectos sugeridos',
+    personal: 'Mis proyectos',
+  }
+  const links = [{
+    title: 'all',
+    content: 'Todos',
+    icon: <GlobeAltIcon className="h-5 w-5" />,
+    query: 'all',
+  }, {
+    title: 'personal',
+    content: 'Mis proyectos',
+    icon: <ListBulletIcon className="h-5 w-5" />,
+    query: 'personal',
+  }, {
+    title: 'suggested',
+    content: 'Proyectos sugeridos',
+    icon: <StarIcon className="h-5 w-5" />,
+    query: 'suggested',
+  }]
 
   return (
     <>
@@ -42,11 +64,23 @@ export default function PageContent({ filter, projects, personalProjects, sugges
           </Button>
         </Link>
       </PageTitle>
-      <PageNav
-        filter={filter}
-        search={search}
-        onSearch={setSearch}
-      />
+      <PageNav dropdownLabel={`Categorías - ${PROJECTS_TAB_TRANSLATION[filter as ProjectsTab]}`} search={search} onSearch={setSearch}>
+        {links.map((link) => {
+          return (
+            <Link
+              key={link.title}
+              href={{
+                pathname: '/home/projects',
+                query: { filter: link.query },
+              }}
+              className={clsx('btn', link.title === filter ? 'btn-primary hover:btn-ghost' : 'hover:btn-primary')}
+            >
+              {link.icon}
+              {link.content}
+            </Link>
+          )
+        })}
+      </PageNav>
       <ProjectsCard
         search={search}
         projects={PROJECT_TABS[filter as ProjectsTab]}
