5ef1f602e25d95e93f8f2bdf29bdf005bd02646e
esteban-florez <eflorez077@gmail.com>
2023-10-14 21:20:18 -0400

    aguacate

--- prisma/seeders/19_intership.ts
+++ prisma/seeders/19_intership.ts
@@ -3,27 +3,25 @@ import { seederQueries } from '../seed'
 import collect from '@/lib/utils/collection'
 import numbers from '@/lib/utils/number'
 import types from '@/lib/utils/types'
-import { Stage } from '@prisma/client'
+import { Status } from '@prisma/client'
 
 export default async function intership() {
-  const persons = await prisma.person.findMany({ select: { id: true } })
-  const institutes = await prisma.institute.findMany({ select: { id: true } })
-  const companies = await prisma.company.findMany({ select: { id: true } })
-  const grades = await prisma.grade.findMany({ select: { id: true } })
-  const categories = await prisma.category.findMany({ select: { id: true } })
+  const persons = collect(await prisma.person.findMany({ select: { id: true } }))
+  const institutes = collect(await prisma.institute.findMany({ select: { id: true } }))
+  const grades = collect(await prisma.grade.findMany({ select: { id: true } }))
+  const categories = collect(await prisma.category.findMany({ select: { id: true } }))
 
   for (let i = 0; i < seederQueries.internships; i++) {
     await prisma.internship.create({
       data: {
         hours: numbers(20, 60).random(),
         completed: numbers(10, 30).random(),
-        stage: types(Stage).random(),
-        personId: collect(persons).random().first().id,
-        instituteId: collect(institutes).random().first().id,
-        companyId: collect(companies).random().first().id,
-        gradeId: collect(grades).random().first().id,
+        status: types(Status).random(),
+        personId: persons.random().first().id,
+        instituteId: institutes.random().first().id,
+        gradeId: grades.random().first().id,
         categories: {
-          connect: collect(categories).random(5).all(),
+          connect: categories.random(5).all(),
         },
       },
     })
