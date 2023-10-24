80cb9a46471a9cd457bef04f774b60c55ddf7b4c
JesusMiguel03 <jmdefreitas10@gmail.com>
2023-10-24 02:39:12 -0400

    Footer added

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -28,6 +28,16 @@ export default async function MainLayout({
       <div className="relative w-full flex-col sm:static">
         <Navbar />
         <main className="bg-base-200">{children}</main>
+        <footer className="footer text-center footer-center fixed bottom-0 p-2 border-t">
+          <div className="flex flex-col sm:flex-row sm:-ms-[17.8rem]">
+            <img
+              src="/logo.png"
+              alt=""
+              className="sm:order-0 w-8"
+            />
+            <p>Copyright © 2023 - Todos los derechos reservados | Novaship</p>
+          </div>
+        </footer>
       </div>
     </div>
   )
