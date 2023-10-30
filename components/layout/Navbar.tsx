2b86ea9b43176e53b550cced5817a9a8ac228e1e
MariYaqueno <myaqueno@gmail.com>
2023-10-30 09:54:07 -0400

    Card minor changes

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -13,13 +13,13 @@ export default function Navbar() {
       <div className="w-full sm:navbar-start">
         <Link
           href="/home"
-          className="mx-auto text-3xl font-bold text-primary sm:mx-0 sm:p-2"
+          className="-mb-1.5 mx-auto text-3xl font-bold text-primary sm:-mb-0 sm:mx-0 sm:p-2"
         >
           Novaship
         </Link>
       </div>
       <div className="w-full text-sm sm:navbar-end sm:text-base">
-        <div className="mx-auto flex flex-col items-center px-5 sm:mx-0 ">
+        <div className="mx-auto flex flex-col items-center px-5 sm:mx-0">
           <p>{date}</p>
         </div>
         <div className="absolute right-2 top-0 sm:static">
