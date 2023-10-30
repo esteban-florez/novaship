0026b3da3a3bb1acb5981c4058206d03a964a06a
MariYaqueno <myaqueno@gmail.com>
2023-10-30 09:40:09 -0400

    Carousel minor changes

--- components/carousel/Content.tsx
+++ components/carousel/Content.tsx
@@ -1,5 +1,6 @@
 import AvatarIcon from '@/components/AvatarIcon'
 import Link from 'next/link'
+import InlineList from '../InlineList'
 
 type Props = {
   title: string
@@ -20,38 +21,33 @@ type Props = {
 
 export default function Content(props: Props) {
   return (
-    <section className="w-full h-72 md:w-[36.5rem] flex">
-      <div className="bg-gray-900/70 flex flex-col justify-center p-7 pb-16">
-        <h3 className="line-clamp-1 text-lg font-bold text-white sm:text-2xl">{props.title}</h3>
+    <section className="h-full w-full bg-gray-900/70 flex flex-col justify-center p-4 pb-16 md:p-7 md:pt-0 md:w-[36rem]">
+      <div className="mb-2 line-clamp-1 md:w-[30rem]">
+        <h3 className="-mb-1 text-xl font-bold text-white sm:text-2xl">{props.title}</h3>
         {props.destination === 'offers' &&
-          <ul className="flex flex-row flex-wrap font-semibold text-white/75">
-            {props.categories.map(category => {
-              return (
-                <li
-                  key={category.id}
-                  className="me-1 cursor-pointer text-base after:text-white/75 after:content-['|'] last:after:content-[] hover:text-white/40"
-                >
-                  {category.title}
-                </li>
-              )
-            })}
-          </ul>}
-        <p className="line-clamp-3 py-2 text-white sm:pt-7 xl:py-3">{props.description}</p>
-        <div className="flex flex-col justify-between gap-4 sm:flex-row md:items-center lg:gap-6">
-          {props.destination === 'offers' &&
-            <div className="order-1 flex flex-row items-center justify-start gap-2 rounded-lg lg:order-none">
-              <AvatarIcon className="bg-black text-white" />
-              <div className="flex flex-col">
-                <h5 className="font-bold text-white lg:text-base">{props.owner}</h5>
-                <small className="-mt-1 text-white">{props.location}</small>
-              </div>
-            </div>}
-          <Link href={props.destination === 'home' ? props.link : `/home/offers/${props.id}`}>
-            <button className="btn btn-wide btn-secondary hover:bg-white hover:text-neutral-600 hover:border-secondary">
-              Ver más
-            </button>
-          </Link>
-        </div>
+          <InlineList
+            items={props.categories.map(category => category.title)}
+            className="text-white/70"
+            hideList
+          />}
+      </div>
+      <p className="line-clamp-2 py-2 text-white sm:py-4 border-t-2 border-neutral-300">
+        {props.description}
+      </p>
+      <div className="flex flex-col gap-3 w-full">
+        {props.destination === 'offers' &&
+          <div className="flex flex-row items-center justify-start gap-2 rounded-lg lg:order-none">
+            <AvatarIcon className="bg-black text-white" />
+            <div className="flex flex-col">
+              <h5 className="font-bold text-white lg:text-base">{props.owner}</h5>
+              <small className="-mt-1 text-white">{props.location}</small>
+            </div>
+          </div>}
+        <Link href={props.destination === 'home' ? props.link : `/home/offers/${props.id}`}>
+          <button className="btn btn-wide btn-secondary hover:bg-white hover:text-neutral-600 hover:border-secondary">
+            Ver más
+          </button>
+        </Link>
       </div>
     </section>
     // <section className="flex w-full justify-center">
