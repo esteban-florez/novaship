0609cf492c9513364681aee1ec742532886e9f8f
JesusMiguel03 <jmdefreitas10@gmail.com>
2023-12-30 18:13:46 -0400

    fix: previous format addition errors

--- app/home/internships/select/PersonsList.tsx
+++ app/home/internships/select/PersonsList.tsx
@@ -1,5 +1,5 @@
 import AvatarIcon from '@/components/AvatarIcon'
-import { ci } from '@/lib/utils/text'
+import { format } from '@/lib/utils/text'
 import { type Person } from '@prisma/client'
 import Link from 'next/link'
 
@@ -8,32 +8,36 @@ type Props = React.PropsWithChildren<{
 }>
 
 export default function PersonsList({ persons }: Props) {
-  return (
-    persons.map(person => (
-      <div key={person.id} className="flex flex-col items-center justify-center gap-1 rounded-lg border-l-4 border-l-primary bg-white p-2 shadow-md md:flex-row md:justify-between">
-        <div className="flex gap-2">
-          <AvatarIcon image={person.image} className="h-12 w-12" />
-          <div>
-            <h3 className="mb-0 text-xl font-bold">{person.name}</h3>
-            <p>CI: {ci(person.ci)}</p>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <Link
-            className="btn btn-primary"
-            href={`/home/internships/create?person=${person.id}`}
-          >
-            Inscribir
-          </Link>
-          <button
-            className="btn btn-secondary"
-            // TODO -> profile link
-            // href={`/home/persons/${person.id}`}
-          >
-            Perfil
-          </button>
+  return persons.map((person) => (
+    <div
+      key={person.id}
+      className="flex flex-col items-center justify-center gap-1 rounded-lg border-l-4 border-l-primary bg-white p-2 shadow-md md:flex-row md:justify-between"
+    >
+      <div className="flex gap-2">
+        <AvatarIcon
+          image={person.image}
+          className="h-12 w-12"
+        />
+        <div>
+          <h3 className="mb-0 text-xl font-bold">{person.name}</h3>
+          <p>CI: {format(person.ci, 'ci')}</p>
         </div>
       </div>
-    ))
-  )
+      <div className="flex gap-2">
+        <Link
+          className="btn btn-primary"
+          href={`/home/internships/create?person=${person.id}`}
+        >
+          Inscribir
+        </Link>
+        <button
+          className="btn btn-secondary"
+          // TODO -> profile link
+          // href={`/home/persons/${person.id}`}
+        >
+          Perfil
+        </button>
+      </div>
+    </div>
+  ))
 }
